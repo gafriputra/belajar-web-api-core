3849719c03c023d57d17ddc74e2745b27438d055
HESYSTEM <HESYSTEM@SYSTEM-08241.hartonoelektronika.com>
2021-08-25 16:36:58 +0700

    First Commit

---
+++ BookStoreWebApi/Controllers/AuthorController.cs
@@ -0,0 +1,45 @@
+﻿using BookStoreWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+      
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+            using (var context = new BookStoresDBContext())
+            {
+                //get all authors
+                //return context.Authors.ToList();
+
+                //add a author
+                //Author author = new Author();
+                //author.FirstName = "Gafri";
+                //author.LastName = "Putra";
+                //context.Authors.Add(author);
+
+                //updated author
+                //Author author = context.Authors.Where(a => a.FirstName == "Gafri").FirstOrDefault();
+                //author.Phone = "123456";
+
+                //deleted author
+                Author author = context.Authors.Where(a => a.FirstName == "Gafri").FirstOrDefault();
+                context.Authors.Remove(author);
+
+                context.SaveChanges();
+
+                //get author where query
+                return context.Authors.Where(a => a.FirstName == "Gafri").ToList();
+            }
+        }
+    }
+}
---
+++ BookStoreWebApi/Controllers/PublishersController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStoreWebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookStoreWebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        private readonly BookStoresDBContext _context;
+
+        public PublishersController(BookStoresDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Publishers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Publisher>>> GetPublishers()
+        {
+            return await _context.Publishers.ToListAsync();
+        }
+
+        // GET: api/Publishers/5
+        [HttpGet("GetPublisherDetails/{id}")]
+        public async Task<ActionResult<Publisher>> GetPublisherDetails(int id)
+        {
+            //Eager Loading
+            /*var publisher = await _context.Publishers
+                                    .Include(pub => pub.Books)
+                                        .ThenInclude(book => book.Sales)
+                                    .Include(pub => pub.Users)
+                                        .ThenInclude(user => user.Job)
+                                    .Where(pub => pub.PubId == id)
+                                    .FirstOrDefaultAsync();*/
+            //Explicit Loading
+            var publisher = await _context.Publishers.SingleAsync(pub => pub.PubId == id);
+
+            _context.Entry(publisher)
+                    .Collection(pub => pub.Users)
+                    .Query()
+                    .Where(usr => usr.EmailAddress.Contains("a"))
+                    .Load();
+
+            _context.Entry(publisher)
+                    .Collection(pub => pub.Books)
+                    .Query()
+                    .Include(book => book.Sales)
+                    .Load();
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return publisher;
+        }
+
+        // GET: api/Publishers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Publisher>> GetPublisher(int id)
+        {
+            var publisher = await _context.Publishers.FindAsync(id);
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return publisher;
+        }
+
+        // PUT: api/Publishers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPublisher(int id, Publisher publisher)
+        {
+            if (id != publisher.PubId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(publisher).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Publishers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Publisher>> PostPublisher(Publisher publisher)
+        {
+            _context.Publishers.Add(publisher);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPublisher", new { id = publisher.PubId }, publisher);
+        }
+
+        // DELETE: api/Publishers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublisher(int id)
+        {
+            var publisher = await _context.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Publishers.Remove(publisher);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return _context.Publishers.Any(e => e.PubId == id);
+        }
+    }
+}
